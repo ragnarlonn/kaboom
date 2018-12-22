ef6c79b9001345eab426710f11b181999be9135b
Ragnar Lonn <ragnar@lonn.org>
2018-12-22 22:11:32 +0100

    Seems functional, looks decent

--- src/kaboom.h
+++ src/kaboom.h
@@ -15,16 +15,24 @@ const char *title[] = {
     "|  .  \\   /  _____  \\  |  |_)  | |  `--'  | |  `--'  | |  |  |  | ",
     "|__|\\__\\ /__/     \\__\\ |______/   \\______/   \\______/  |__|  |__| "
 };
+const char *title2[] = {
+" __ _   __   ____   __    __   _  _ ",
+"(  / ) / _\\ (  _ \\ /  \\  /  \\ ( \\/ )",
+" )  ( /    \\ ) _ ((  O )(  O )/ \\/ \\",
+"(__\\_)\\_/\\_/(____/ \\__/  \\__/ \\_)(_/"
+};
 
 const char *html1 = "\
   <!DOCTYPE HTML>\
   <html>\
   <head><title>Kaboom fire control</title></head>\
   <body>\
-  <style>body {display: flex; font-size: 20px; flex-direction: column; justify-content: center; align-items: center; }\
-  table {width:80%; border:1px solid black; border-collapse: collapse;}\
-  td {border: 1px solid black; border-collapse: collapse;}\
-  button {margin: 10px;}\
+  <style>body {display: flex; font-size: 20px; flex-direction: column; align-items: center;}\
+  table {display: flex; flex-direction: column; align-items: center; width:80%; border-collapse: collapse;}\
+  td {border: 1px solid black; border-collapse: collapse; padding: 20px; min-width: 50px; text-align: center;}\
+  button {padding: 15px 30px; border-radius: 10px; border: 1px solid red; background-image: linear-gradient(to bottom, rgba(255,120,120), rgba(255,40,40)); font-size: 16px; font-weight:bold;}\
+  button:hover {opacity: 0.8;}\
+  button:disabled{opacity: 0.5;}\
   </style>\
   <table>\
   <tr><th>Name</th><th>Pin #</th><th>Firing</th><th>Fired</th><th></th></tr>\
@@ -33,11 +41,12 @@ const char *html1 = "\
 const char *html2 = "\
   </table>\
   <script type=\"text/javascript\">\
-    function fire(e) {\
+  var buttonPushed;\
+  function fire(e) {\
     var pin = e.currentTarget.id.substring(6);\
-    document.getElementById('firing' + pin).backgroundColor = \"#red\";\
+    document.getElementById('firing' + pin).style.backgroundColor = \"orange\";\
     setTimeout(function(p) { return function() {\
-      document.getElementById('firing' + p).backgroundColor = \"#white\";\
+      document.getElementById('firing' + p).style.backgroundColor = \"white\";\
     };}(pin), 2000);\
     var xhr = new XMLHttpRequest();\
     xhr.onreadystatechange = function(p) {\
@@ -45,12 +54,12 @@ const char *html2 = "\
         if (this.readyState == XMLHttpRequest.DONE) {\
           if (this.status == 200) {\
             document.getElementById('button' + p).disabled = true;\
-            document.getElementById('fired' + p).backgroundColor = \"#green\";\
-            document.getElementById('fired' + p).color = \"#black\";\
+            document.getElementById('fired' + p).style.backgroundColor = \"#8f8\";\
+            document.getElementById('fired' + p).color = \"black\";\
             document.getElementById('fired' + p).innerHTML = \"<b>OK</b>\";\
           } else {\
-            document.getElementById('fired' + p).backgroundColor = \"#red\";\
-            document.getElementById('fired' + p).color = \"#black\";\
+            document.getElementById('fired' + p).style.backgroundColor = \"#f88\";\
+            document.getElementById('fired' + p).color = \"black\";\
             document.getElementById('fired' + p).innerHTML = \"<b><i>ERR</b></i>\";\
           }\
         }\
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+
 #include "kaboom.h"
+
+// Do you have a NodeMCU with OLED screen? 
+#define OLED
+// #undef OLED
 
 // Configure your SSID name and password here
 const char *ssid = "Kaboom AP";
 const char *pw = "secret314";
 
-// Configure your firing channels here. Each NodeMCU pin you configure will be set HIGH for 2 seconds
+// Configure your firing channels here. Each NodeMCU pin you configure will be set LOW for 2 seconds
 // after its firing button is clicked in the web UI.
 //
 // Each firing channel should be added as a new line in the array below. Copy/change an existing
@@ -16,17 +21,34 @@ const char *pw = "secret314";
 // Use the NodeMCU GPIO pin number as the pin number here. I.e. Digital pin D0 == 16 (GPIO16)
 // (Google "NodeCU pinout" to get a reference drawing)
 struct DetonatorChannel channels[] = {
-  { 16, "Falcon 9.5", false, false, false, millis() },
-  { 5, "My rocket on pin D1", false, false, false, millis() }
+  { 12, "Falcon 9.5 @D6", false, false, false, millis() },
+  { 13, "My rocket @D7", false, false, false, millis() }
 };
 
+//
 // No more configuration necessary from here on
+//
+
+#ifdef OLED
+#include <Arduino.h>
+#include <U8g2lib.h>
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+#endif
 
 //WiFiServer server(80);
 ESP8266WebServer server(80);
 String httpRequest;
 String htmlout;
 const unsigned int numChannels = (sizeof(channels) / sizeof(struct DetonatorChannel));
+
+#ifdef OLED
+U8X8_SSD1306_128X32_UNIVISION_SW_I2C u8x8(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ 16);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
+#endif
 
 void handleRoot() {
   String s = html1;
@@ -35,12 +57,12 @@ void handleRoot() {
     bool fired = channels[i].fired;
     bool error = channels[i].error;
     snprintf(tmp, 300, "<tr><td>%s</td><td>%d</td>\
-      <td id=\"firing%d\" style=\"background-color:#white; color:#black\"></td>\
+      <td id=\"firing%d\" style=\"background-color:white; color:black\"></td>\
       <td id=\"fired%d\" style=\"background-color: %s; color:%s;\">%s</td>\
       <td><button id=\"button%d\" onclick=\"fire(event)\" %s>fire</button></td></tr>",
       channels[i].name.c_str(), channels[i].pinNumber, 
-      channels[i].pinNumber,
-      channels[i].pinNumber, fired ? (error ? "#red" : "#green") : "#white", "#black", 
+      channels[i].pinNumber, 
+      channels[i].pinNumber, fired ? (error ? "#f88" : "#8f8") : "white", "black", 
       fired ? (error ? "<b><i>ERR</i></b>" : "<b>OK</b>") : "",
       channels[i].pinNumber, fired ? "disabled" : "");
     s += tmp;
@@ -64,17 +86,30 @@ void handleFire() {
   }
   for (uint8_t i = 0; i < numChannels; i++) {
     if (channels[i].pinNumber == pin) {
-      Serial.print("Got FIRE command for pin "); Serial.print(pin); Serial.println(" - EXECUTING");
+      digitalWrite(pin, LOW);
       channels[i].changed = millis();
       channels[i].on = true;
-      digitalWrite(pin, HIGH);
       server.send(200, "text/plain", "KABOOM");
+      Serial.print("Got FIRE command for pin "); Serial.print(pin); Serial.println(" - EXECUTING");
       return;
     }
   }
   server.send(404, "text/plain", "KEEP TRACK OF YOUR FIREWORKS!");
   Serial.print("Got FIRE command for non-existent pin number "); Serial.print(pin); Serial.println(", ignoring");
 }
+
+#ifdef OLED
+void OLEDOutput(const char *s1, const char *s2) {
+  u8x8.clearLine(2);
+  u8x8.clearLine(3);
+  if (strlen(s1) > 0) {
+    u8x8.drawUTF8(0, 2, s1);
+  }
+  if (strlen(s2) > 0) {
+    u8x8.drawUTF8(0, 3, s2);
+  }
+}
+#endif
 
 void setup() {
   delay(1000);
@@ -84,20 +119,44 @@ void setup() {
     Serial.println(title[i]);
   }
   Serial.println();
+  #ifdef OLED
+  u8x8.begin();
+  u8x8.setPowerSave(0);
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
+  u8x8.draw2x2UTF8(0, 0, "Kaboom");
+  OLEDOutput("Starting...", "");
+  delay(2000);
+  #endif
   Serial.println("Hello, this is your friendly nodemcu detonation controller");
   Serial.println("Initializing outputs...");
   for (unsigned int i = 0; i<numChannels; i++) {
     Serial.print("Initializing output pin #"); Serial.print(channels[i].pinNumber); 
     Serial.print(" ("); Serial.print(channels[i].name); Serial.print(") ...");
+    #ifdef OLED
+    String s = "Initializing ";
+    s += String(channels[i].pinNumber);
+    OLEDOutput(s.c_str(), "");
+    delay(500);
+    #endif
     pinMode(channels[i].pinNumber, OUTPUT);
-    digitalWrite(channels[i].pinNumber, LOW);
+    digitalWrite(channels[i].pinNumber, HIGH);
     channels[i].on = channels[i].fired = false;
     Serial.println("ok");
   }
   Serial.println("Init outputs done");
   Serial.print("Setting up AP...");
-  boolean res = WiFi.softAP(ssid, pw);
+  #ifdef OLED
+  OLEDOutput("Init done", "Setting up AP..");
+  delay(2000);
+  #endif
+  boolean res = WiFi.softAP(ssid, pw, 8);
+  //boolean res = WiFi.softAP(ssid);
   if (res == true) {
+    #ifdef OLED
+    String s = WiFi.softAPSSID();
+    String s2 = WiFi.softAPIP().toString();
+    OLEDOutput(s.c_str(), s2.c_str());
+    #endif
     Serial.print("ok, IP="); Serial.print(WiFi.softAPIP()); 
     Serial.print(", SSID="); Serial.print(WiFi.softAPSSID());
     Serial.print(", pw="); Serial.println(pw);
@@ -110,15 +169,16 @@ void setup() {
   }
 }
 
-
+// Release tripped relays after 2 seconds
+// (No point in having 12V through the cables after the firing event is over and done with)
 void release() {
   for (unsigned int i = 0; i < numChannels; i++) {
     if (channels[i].on) {
       if (millis() > (channels[i].changed + 2000)) {
         // switch off relay after 2 secs
         Serial.println("");
-        Serial.print("Setting output pin "); Serial.print(channels[i].pinNumber); Serial.println(" to LOW");
-        digitalWrite(channels[i].pinNumber, LOW);
+        Serial.print("Setting output pin "); Serial.print(channels[i].pinNumber); Serial.println(" to HIGH");
+        digitalWrite(channels[i].pinNumber, HIGH);
         channels[i].on = false;
         channels[i].fired = true;
         channels[i].changed = millis();
@@ -131,3 +191,7 @@ void loop() {
   server.handleClient();
   release();
 }
+
+
+
+
