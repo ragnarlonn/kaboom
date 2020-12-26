9e7a194f9a3e4de1a6ed040e4aa228aec7a1ff6c
Ragnar Lonn <ragnar@lonn.org>
2020-12-26 22:26:13 +0100

    Added -12V toggle and reset function

--- v3/webserver.go
+++ v3/webserver.go
@@ -1,48 +1,68 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	rpio "github.com/stianeikeland/go-rpio"
 	"net/http"
-	"encoding/json"
-	"fmt"
-	"time"
-	"errors"
-	"strings"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type Chan struct {
-	Pin int `json:"pin"`
-	Fired bool `json:"fired"`
-	Firing bool `json:"firing"`
+	Pin           int       `json:"pin"`
+	Fired         bool      `json:"fired"`
+	Firing        bool      `json:"firing"`
 	StartedFiring time.Time `json:"started_firing"`
 }
 
-var ( 
-	chans []*Chan = []*Chan{
-		&Chan{ Pin: 25 },
-		&Chan{ Pin: 24 },
-		&Chan{ Pin: 21 },
-		&Chan{ Pin: 12 },
-		&Chan{ Pin: 23 },
-		&Chan{ Pin: 16 },
-		&Chan{ Pin: 18 },
-		&Chan{ Pin: 20 },
-	}
+type State struct {
+	Minus12VPin     int     `json:"minus12vpin"`
+	Minus12VEnabled bool    `json:"minus12venabled"`
+	Channels        []*Chan `json:"channels"`
+}
+
+var (
+	state           State
 	ErrChanNotFound error = errors.New("channel not found")
-	ErrChanFiring error = errors.New("channel already firing")
-	debuglevel int = 1
+	ErrChanFiring   error = errors.New("channel already firing")
+	debuglevel      int   = 1
 )
+
+func resetState() {
+	state = State{
+		Minus12VPin:     4,
+		Minus12VEnabled: false,
+		Channels: []*Chan{
+			&Chan{Pin: 25},
+			&Chan{Pin: 24},
+			&Chan{Pin: 21},
+			&Chan{Pin: 12},
+			&Chan{Pin: 23},
+			&Chan{Pin: 16},
+			&Chan{Pin: 18},
+			&Chan{Pin: 20},
+		},
+	}
+	// Set -12V pin to LOW
+	p := rpio.Pin(state.Minus12VPin)
+	p.Mode(rpio.Output)
+	p.Write(rpio.Low)
+	// Set all channels to LOW
+	for _, ch := range state.Channels {
+		p := rpio.Pin(ch.Pin)
+		p.Mode(rpio.Output)
+		p.Write(rpio.Low)
+	}
+}
 
 func main() {
 	if err := rpio.Open(); err != nil {
 		panic(fmt.Sprintf("Failed to init go-rpio: %s\n", err.Error()))
 	}
-	for _, ch := range chans {
-		p := rpio.Pin(ch.Pin)
-		p.Mode(rpio.Output)
-		p.Write(rpio.Low)
-	}
+	resetState()
 	http.HandleFunc("/", HandleRequest)
 	http.ListenAndServe(":8888", nil)
 }
@@ -50,6 +70,7 @@ func main() {
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Got request for %s\n", r.URL.Path)
 	if strings.HasPrefix(r.URL.Path, "/fire") {
+		// /fire fires one (/fire?chan=1) or more (/fire?chan=1&chan=2) channels
 		r.ParseForm()
 		if chNumbers, ok := r.Form["chan"]; ok {
 			for _, chStr := range chNumbers {
@@ -59,20 +80,34 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Missing chan=n/test query parameter", http.StatusBadRequest)
 			return
 		}
-		if jsonStr, err := json.Marshal(chans); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+	} else if strings.HasPrefix(r.URL.Path, "/reset") {
+		// /reset resets state to startup state
+		resetState()
+	} else if strings.HasPrefix(r.URL.Path, "/minus12vtoggle") {
+		// /minus12vtoggle toggles 12V- on/off
+		state.Minus12VEnabled = !state.Minus12VEnabled
+		p := rpio.Pin(state.Minus12VPin)
+		if state.Minus12VEnabled {
+			p.Write(rpio.High)
 		} else {
-			w.Header().Set("Content-Type", "application/json")
-			fmt.Fprint(w, jsonStr)
+			p.Write(rpio.Low)
 		}
 	} else {
 		DoIndex(w, r)
+		return
+	}
+	if jsonStr, err := json.Marshal(state); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, jsonStr)
+		fmt.Printf("Writing:\n%s\n", jsonStr)
 	}
 }
 
 func _fireStop(chNumberInternal int) {
-	c := chans[chNumberInternal]
+	c := state.Channels[chNumberInternal]
 	fmt.Printf("%s _fireStop: turning off channel %d (pin %d)\n", time.Now().String(), chNumberInternal, c.Pin)
 	p := rpio.Pin(c.Pin)
 	p.Write(rpio.Low)
@@ -81,7 +116,7 @@ func _fireStop(chNumberInternal int) {
 }
 
 func _fireStart(chNumberInternal int) error {
-	c := chans[chNumberInternal]
+	c := state.Channels[chNumberInternal]
 	fmt.Printf("%s _fireStart: got told to fire internal channel %d (pin %d)\n", time.Now().String(), chNumberInternal, c.Pin)
 	if !c.Firing {
 		fmt.Printf("_fireStart: channel %d was not firing, proceeding with fire\n", chNumberInternal)
@@ -101,7 +136,7 @@ func _fireStart(chNumberInternal int) error {
 func DoFire(w http.ResponseWriter, chStr string) {
 	fmt.Printf("DoFire: trying to fire channel %s\n", chStr)
 	if chStr == "test" {
-		for chNumber, _ := range chans {
+		for chNumber, _ := range state.Channels {
 			if err := _fireStart(chNumber); err != nil {
 				http.Error(w, fmt.Sprintf("Warning: channel %d was already firing", chNumber+1), 218)
 			}
@@ -112,23 +147,21 @@ func DoFire(w http.ResponseWriter, chStr string) {
 			http.Error(w, "Bad request: no such channel", http.StatusBadRequest)
 			return
 		}
-		if chNumber < 1 || chNumber > len(chans) {
+		if chNumber < 1 || chNumber > len(state.Channels) {
 			http.Error(w, "Bad request: no such channel", http.StatusBadRequest)
 			return
 		}
-		if err = _fireStart(chNumber-1); err != nil {
+		if err = _fireStart(chNumber - 1); err != nil {
 			http.Error(w, err.Error(), 555)
 			return
 		}
 	}
 }
 
-
-
 func DoIndex(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, html1)
-	for chNum, chOb := range chans {
-		outStr := strings.Replace(html2, "NAME", fmt.Sprintf("Ch %d", chNum+1),1)
+	for chNum, chOb := range state.Channels {
+		outStr := strings.Replace(html2, "NAME", fmt.Sprintf("Ch %d", chNum+1), 1)
 		outStr = strings.ReplaceAll(outStr, "CHAN", fmt.Sprintf("%d", chNum+1))
 		if chOb.Fired {
 			outStr = strings.Replace(outStr, "DISABLED", "disabled", 1)
@@ -139,8 +172,6 @@ func DoIndex(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprint(w, html3)
 }
-
-
 
 const html1 = `
 <!DOCTYPE HTML>
@@ -212,6 +243,8 @@ const html2 = `
 const html3 = `
 		</table>
 		<button onclick="testFire(event);">Fire all channels</button>
+		<button id="toggle12vminus" onclick="toggle12vminus(event);">-12V is <b>OFF</b></button>
+		<button onclick="reset(event);">Reset system state</button>
 		<script type="text/javascript">
 			async function testFire(e) {
 				for (var chan = 1; chan <= 8; chan++) {
@@ -248,10 +281,36 @@ const html3 = `
 				var chan = e.currentTarget.id.substring(6);
 				_fire(chan);
 			}
+			function toggle12vminus(e) {
+				var xhr = new XMLHttpRequest();
+				xhr.onreadystatechange = function() {
+					if (this.readyState == XMLHttpRequest.DONE) {
+						if (this.status == 200) {
+							state = JSON.parse(this.responseText);
+							document.getElementById("toggle12vminus").innerHTML = "-12V is <b>" +
+								(state.minus12venabled ? "ON" : "OFF") + "</b>";
+						}
+					}
+				};
+				xhr.open("GET", "/minus12vtoggle", true);
+				xhr.send();
+
+			}
+			function reset(e) {
+				var xhr = new XMLHttpRequest();
+				xhr.onreadystatechange = function() {
+					if (this.readyState == XMLHttpRequest.DONE) {
+						if (this.status == 200) {
+							location.reload();
+						}
+					}
+				};
+				xhr.open("GET", "/reset", true);
+				xhr.send();
+
+			}
 		</script>
 	</body>
 </html>
 
 `
-
-
